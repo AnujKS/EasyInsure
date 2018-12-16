30acab4745d641b0b2e7f742e502aaf561506d06
hitesh sethiya <hiteshsethiya@gmail.com>
2018-12-16 11:21:31 +0530

    model

--- app/src/main/java/com/bumperjumper/android/easyinsure/model/PolicyFormPojo.java
+++ app/src/main/java/com/bumperjumper/android/easyinsure/model/PolicyFormPojo.java
@@ -1,19 +1,35 @@
 package com.bumperjumper.android.easyinsure.model;
 
+import android.os.Build;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.time.LocalDate;
+
+import androidx.annotation.RequiresApi;
+
 public class PolicyFormPojo implements Parcelable {
-    public String name;
-    public String vehicleType;
-    public String displacement;
+
     public String registrationNumber;
+    public String name;
+    public String description;
+    public String registrationYear;
+    public String vehicleCompany;
+    public String vehicleModel;
+    public String displacement;
+    public String makeDescription;
+    public String modelDescription;
+    public String chassisNumber;
+    public String numberOfSeats;
+    public String colour;
+    public String engineNumber;
+    public String fuelType;
+    public String registrationDate;
+    public String location;
+    public String vehicleType;
+    public String premium;
     public String policyDate;
     public String policyExpiryDate;
-    public String chassisNumber;
-    public String engineNumber;
-    public String vehicleCompany;
-    public String premium;
 
 
     @Override
@@ -23,35 +39,59 @@ public class PolicyFormPojo implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.name);
-        dest.writeString(this.vehicleType);
-        dest.writeString(this.displacement);
+
         dest.writeString(this.registrationNumber);
-        dest.writeString(this.policyDate);
-        dest.writeString(this.policyExpiryDate);
-        dest.writeString(this.chassisNumber);
-        dest.writeString(this.engineNumber);
+        dest.writeString(this.name);
+        dest.writeString(this.description);
+        dest.writeString(this.registrationYear);
         dest.writeString(this.vehicleCompany);
+        dest.writeString(this.vehicleModel);
+        dest.writeString(this.displacement);
+        dest.writeString(this.makeDescription);
+        dest.writeString(this.modelDescription);
+        dest.writeString(this.chassisNumber);
+        dest.writeString(this.numberOfSeats);
+        dest.writeString(this.colour);
+        dest.writeString(this.engineNumber);
+        dest.writeString(this.fuelType);
+        dest.writeString(this.registrationDate);
+        dest.writeString(this.location);
+        dest.writeString(this.vehicleType);
         dest.writeString(this.premium);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public PolicyFormPojo() {
+        this.policyDate = LocalDate.now().toString();
+        this.policyDate = LocalDate.now().plusDays(365).toString();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     protected PolicyFormPojo(Parcel in) {
-        this.name = in.readString();
-        this.vehicleType = in.readString();
-        this.displacement = in.readString();
-        this.registrationNumber = in.readString();
-        this.policyDate = in.readString();
-        this.policyExpiryDate = in.readString();
-        this.chassisNumber = in.readString();
-        this.engineNumber = in.readString();
-        this.vehicleCompany = in.readString();
-        this.premium = in.readString();
+        this();
+
+        this.registrationNumber= in.readString();
+        this.name= in.readString();
+        this.description= in.readString();
+        this.registrationYear= in.readString();
+        this.vehicleCompany= in.readString();
+        this.vehicleModel= in.readString();
+        this.displacement= in.readString();
+        this.makeDescription= in.readString();
+        this.modelDescription= in.readString();
+        this.chassisNumber= in.readString();
+        this.numberOfSeats= in.readString();
+        this.colour= in.readString();
+        this.engineNumber= in.readString();
+        this.fuelType= in.readString();
+        this.registrationDate= in.readString();
+        this.location= in.readString();
+        this.vehicleType= in.readString();
+        this.premium= in.readString();
     }
 
     public static final Parcelable.Creator<PolicyFormPojo> CREATOR = new Parcelable.Creator<PolicyFormPojo>() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         public PolicyFormPojo createFromParcel(Parcel source) {
             return new PolicyFormPojo(source);
